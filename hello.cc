d41b79a30b1c57d389312beff78c9f98706a2ded
bekzaya <1910510690@qq.com>
2017-02-01 16:48:39 +0800

    hello world

---
+++ hello.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    hello.cc
+ /// @author  bek(1910510690@qq.com)
+ /// @date    2017-02-01 16:43:38
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+int main(viod)
+{
+	printf("hello world\n");
+	return 0;
+}
